d8fb80da91c05a5b862bbb128d6936078c32e762
vinibispo <vini.bispo015@gmail.com>
2020-11-17 07:15:43 -0300

    Adding tests on model

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be invalid if user has no password" do
+    user = build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+  it "should be valid if user has password filled bigger than 8 characters" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  it "should not be valid if user has password filled, but is smaller than 8 characters" do
+    user = build(:user, password: FFaker::Internet.password(1,6))
+    expect(user).to_not be_valid
+  end
+  it "should not be valid if user has no email filled" do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
 end
