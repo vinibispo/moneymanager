68aae0a10d3fe97ff3d111337be784e4baf1f1a1
vinibispo <vini.bispo015@gmail.com>
2020-11-17 07:16:12 -0300

    Adding requests tests

--- backend/spec/requests/users_spec.rb
+++ backend/spec/requests/users_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "Should return all users" do
       create_list(:user, 10)
       get "#{users_path}.json"
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(json.length).to be(10)
+    end
+    it "should return just a user" do
+      user = create(:user)
+      get "#{user_path(user.id)}.json"
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(json["email"]).to eq user[:email]
     end
   end
 end
