ebb29e20e71ac1fcd14c8d7285dc3eb8402ed1ce
vinibispo <vini.bispo015@gmail.com>
2020-11-17 07:14:41 -0300

    Fixing password_digest to password

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { FFaker::Name.name }
     email { FFaker::Internet.email }
-    password_digest { FFaker::CheesyLingo.words }
+    password { FFaker::Internet.password(8) }
   end
 end
