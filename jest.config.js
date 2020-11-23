8091ae05c0081ff5fe3f2a50e86f8215e1d7d57b
vinibispo <vini.bispo015@gmail.com>
2020-11-23 08:18:48 -0300

    I can do it

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   bail: true,
   clearMocks: true,
+  preset: 'react-native',
+  testEnvironment: 'jsdom',
   projects: ['<rootDir>/packages/**/jest.config.js'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
