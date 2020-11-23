89b5e2f0205369e3faeb8ea78e93c14e2655a2b7
vinibispo <vini.bispo015@gmail.com>
2020-11-23 09:34:44 -0300

    Setting up rspec

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
-yarn jest --findRelatedTests packages/mobile packages/frontend && cd packages/backend && bundle exec rspec
+yarn jest --findRelatedTests packages/mobile packages/frontend && cd packages/backend && bundle exec rspec $(git ls-files --modified spec)
 cd ../..
