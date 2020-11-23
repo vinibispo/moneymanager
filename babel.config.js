27cccbf9d5087287ece4fd66a230a0187720a4e8
vinibispo <vini.bispo015@gmail.com>
2020-11-23 09:09:30 -0300

    Tests works

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,10 @@ module.exports = {
     "@babel/preset-env",
     "@babel/preset-react",
     "module:metro-react-native-babel-preset",
-    "@babel/preset-typescript",
+    "@babel/preset-typescript"
   ],
   plugins: [
+    ["styled-components", { ssr: true }], "inline-react-svg",
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-transform-modules-commonjs",
     "@babel/plugin-transform-runtime",
--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { Container } from "../styles/pages/Home";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
 }
