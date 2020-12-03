2d286febedcf313dc4e441c598829b5680191259
vinibispo <vini.bispo015@gmail.com>
2020-12-03 15:12:01 -0300

    test(backend): trying to add tests on generator

---
+++ packages/backend/spec/lib/generators/service/service_generator_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# require 'generator_spec'
+# require 'rails_helper'
+# require 'spec_helper'
+
+# describe ServiceGenerator, type: :generator do
+# destination File.expand_path("../../tmp", __FILE__)
+
+# before do
+# prepare_destination
+# run_generator
+# end
+
+# it 'creates a test initializer' do
+# assert_file 'app/services/api_service.rb', 'class ApiService'
+# end
+# end
