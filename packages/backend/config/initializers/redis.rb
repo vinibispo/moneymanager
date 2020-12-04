02248a74e41a9bfda5fdad85da658b20ac08a85c
vinibispo <vini.bispo015@gmail.com>
2020-12-04 15:37:23 -0300

    fix(backend): fixing tests that were broken

--- packages/backend/config/initializers/redis.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-if Rails.env.production?
-  Redis.new(url: ENV['REDIS_URL'])
-else
-  redis_config = YAML.safe_load(File.open(Rails.root.join('config/redis.yml'))).symbolize_keys[Rails.env.to_sym]
-  Redis.new(redis_config)
-end
